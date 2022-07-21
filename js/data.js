83607d6daeac2f89f1d484b7aaab227ca97bd2b7
Shan10Code <jojogithub@gmail.com>
2022-07-21 13:14:47 +0530

    Updated memory

--- js/data.js
+++ js/data.js
@@ -1,2 +1,2 @@
-var blogContent =  /*startIndex*/['', '<div id="1" class="post"><b><u>Test-Post</u></b> Posted on 2022-07-21 12:11<br><h1>Hi this is a test post!</h1>\n<ul>\n<li>bing</li>\n<li>chill</li>\n<li>ING</li>\n</ul></div>']/*endIndex*/
-function gimmeContent(){return blogContent}
+var blogContent =  /*startIndex*/['', '<div id="1" class="post"><b><u>Test-Post</u></b> Posted on 2022-07-21 12:11<br><h1>Hi this is a test post!</h1>\n<ul>\n<li>bing</li>\n<li>chill</li>\n<li>ING</li>\n</ul></div>', '<div id="2" class="post"><b><u>images-work-too</u></b> Posted on 2022-07-21 12:33<br><h2>Sometimes you\'ve got to close a door to open a window</h2>\n<p><img alt="Some Image" src="https://www.rollingstone.com/wp-content/uploads/2019/05/TylerTheCreator_SamRock.jpg?resize=1800,1200&amp;w=1200" /></p></div>']/*endIndex*/
+function loadContent(){return blogContent}
