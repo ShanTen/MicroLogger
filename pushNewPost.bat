9d1b555a947d16e87fe937c50713b555df9b31cb
Shan10Code <jojogithub@gmail.com>
2022-07-21 13:15:18 +0530

    Updated batch

--- pushNewPost.bat
+++ pushNewPost.bat
@@ -5,5 +5,6 @@ popd
 git add .\WriteBlogHere\*
 git commit -m "Posted Something New!"
 git add .\metaScripts\memory.json
+git add .\js\data.js
 git commit -m "Updated memory"
 git push -u origin 'master' rem this is a mildly stupid thing I did, I named my master branch 'master' instead of master 
