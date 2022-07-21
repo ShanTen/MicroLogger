2206c7f78899eb0c88cba2cbef685edbc915a3da
Shan10Code <jojogithub@gmail.com>
2022-07-21 13:11:06 +0530

    Cleaned up code and renamed main function

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 function main(){    
-    var formattedContent = gimmeContent();
+    var formattedContent = loadContent();
     var toLoad = document.getElementsByClassName("blogPart")[0]
     for(x of formattedContent.reverse()){ //reversed so that newest posts pop up first
         toLoad.innerHTML += `<p class = "Loaded">${x}</p><br><br>`;
