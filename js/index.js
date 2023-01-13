e4d7bd0debcaecbd4f69f7eb5419ae7e4598b925
Shan10Code <jojogithub@gmail.com>
2023-01-13 11:27:32 +0530

    Updated scripts

--- js/index.js
+++ js/index.js
@@ -1,6 +1,7 @@
 function main(){    
     var formattedContent = loadContent();
     var toLoad = document.getElementsByClassName("blogPart")[0]
+    console.log("Hellos");
     for(x of formattedContent.reverse()){ //reversed so that newest posts pop up first
         toLoad.innerHTML += `<p class = "Loaded">${x}</p><br><br>`;
     }
